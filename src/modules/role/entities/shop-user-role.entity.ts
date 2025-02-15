6cda9353964bb31a6c38e5bd3d4b9d67976aa7d0
e.hajiabadi <E.hajiabadi@ecoapp.ir>
2025-02-15 18:06:38 +0330

    Update ShopUserRole: Use ManyToOne relationship with Role instead of ManyToMany for single role per user per shop

--- src/modules/role/entities/shop-user-role.entity.ts
+++ src/modules/role/entities/shop-user-role.entity.ts
@@ -1,14 +1,13 @@
 import { BaseEntity } from "src/common/abstracts/base.entity";
-import { Entity, JoinTable, ManyToMany, ManyToOne } from "typeorm";
+import { Entity, ManyToOne } from "typeorm";
 import { Role } from "./role.entity";
 import { Shop } from "src/modules/shop/entities/shop.entity";
 import { User } from "src/modules/user/entities/user.entity";
 
 @Entity("shop_user-roles")
 export class ShopUserRole extends BaseEntity {
-  @ManyToMany(() => Role, (role) => role.users)
-  @JoinTable()
-  roles: Role[];
+  @ManyToOne(() => Role, (role) => role.users)
+  roles: Role;
   @ManyToOne(() => Shop, (shop) => shop.userRoles)
   shop: Shop;
   @ManyToOne(() => User, (user) => user.shopRoles)

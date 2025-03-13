2d06274ce659c7cf0f6d70c9cdeee0ff4d29fd11
e.hajiabadi <E.hajiabadi@ecoapp.ir>
2025-03-13 15:21:23 +0330

    feat: enhance UploadFilesInterceptor to accept custom allowed MIME types

--- src/common/interceptors/uploadFiles.interceptor.ts
+++ src/common/interceptors/uploadFiles.interceptor.ts
@@ -3,18 +3,19 @@ import { FileFieldsInterceptor } from "@nestjs/platform-express";
 import { MulterField } from "@nestjs/platform-express/multer/interfaces/multer-options.interface";
 import { memoryStorage } from "multer";
 
-export function UploadFilesInterceptor(uploadFields: MulterField[]) {
+export function UploadFilesInterceptor(
+  uploadFields: MulterField[],
+  allowedMimeTypes: string[] = [
+    "image/jpeg",
+    "image/png",
+    "video/mp4",
+    "image/jpg",
+  ]
+) {
   return FileFieldsInterceptor(uploadFields, {
     storage: memoryStorage(),
 
     fileFilter: (req, file, callback) => {
-      const allowedMimeTypes = [
-        "image/jpeg",
-        "image/png",
-        "video/mp4",
-        "image/jpg",
-      ];
-
       if (!allowedMimeTypes.includes(file.mimetype))
         callback(new Error("Invalid file type"), false);
 
